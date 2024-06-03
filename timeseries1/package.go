40aa2227db6a6136060b41a9d094b4defa7e814b
advanced-go <mark.wilson.go@outlook.com>
2024-06-03 15:08:53 -0500

    Update package.go

--- timeseries1/package.go
+++ timeseries1/package.go
@@ -7,6 +7,7 @@ import (
 	"github.com/advanced-go/observation/module"
 	"github.com/advanced-go/stdlib/core"
 	json2 "github.com/advanced-go/stdlib/json"
+	"github.com/advanced-go/stdlib/uri"
 	"net/http"
 	"net/url"
 	"strings"
@@ -22,13 +23,14 @@ func errorInvalidURL(path string) *core.Status {
 
 // Get - resource GET
 func Get(ctx context.Context, h http.Header, url *url.URL) ([]Entry, *core.Status) {
-	if url == nil || !strings.HasPrefix(url.Path, module.TimeseriesResource) {
+	if url == nil || !strings.HasPrefix(url.Path, "/"+module.Authority) {
 		return nil, core.NewStatusError(core.StatusInvalidArgument, errors.New(fmt.Sprintf("invalid or nil URL")))
 	}
 	if url.Query() == nil {
 		return nil, core.NewStatusError(core.StatusInvalidArgument, errors.New(fmt.Sprintf("query arguments are nil")))
 	}
-	switch url.Path {
+	p := uri.Uproot(url.Path)
+	switch p.Resource {
 	case module.TimeseriesResource:
 		return get[core.Log](ctx, core.AddRequestId(h), url)
 	default:
@@ -38,7 +40,7 @@ func Get(ctx context.Context, h http.Header, url *url.URL) ([]Entry, *core.Statu
 
 // Put - resource PUT, with optional content override
 func Put(r *http.Request, body []Entry) *core.Status {
-	if r == nil || r.URL == nil || !strings.HasPrefix(r.URL.Path, module.TimeseriesResource) {
+	if r == nil || r.URL == nil || !strings.HasPrefix(r.URL.Path, "/"+module.Authority) {
 		return core.NewStatusError(core.StatusInvalidArgument, errors.New("invalid URL"))
 	}
 	if body == nil {
@@ -50,7 +52,8 @@ func Put(r *http.Request, body []Entry) *core.Status {
 		}
 		body = content
 	}
-	switch r.URL.Path {
+	p := uri.Uproot(r.URL.Path)
+	switch p.Resource {
 	case module.TimeseriesResource:
 		return put[core.Log](r.Context(), core.AddRequestId(r.Header), body)
 	default:
