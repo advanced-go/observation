61ef1c853f8f1f53b5151895701260e2b5d14984
advanced-go <mark.wilson.go@outlook.com>
2024-07-18 11:43:43 -0500

    Update routing.go

--- access1/routing.go
+++ access1/routing.go
@@ -1,9 +1,9 @@
 package access1
 
 type Routing struct {
-	Url            string `json:"url"`
-	Host           string `json:"host"`
-	RouteTo        string `json:"route-to"`
-	RoutingPercent int    `json:"routing-percent"`
-	RoutingCode    string `json:"routing-code"`
+	Url     string `json:"url"`
+	Host    string `json:"host"`
+	To      string `json:"to"` // Primary - secondary
+	Percent int    `json:"percent"`
+	Code    string `json:"code"`
 }
